649e0b5d77d35d9ae4f34330776b50212841896b
Arlindojulio <arlindojr25@hotmail.com>
2022-01-05 22:10:29 +0000

    tentativa de ciclo for para capturar 1ª letra do sobrenome

--- mostra_nome_e_siglas.py
+++ mostra_nome_e_siglas.py
@@ -1,8 +1,8 @@
 nome_completo = str([input('Digite seu nome completo: ')])
-nome_sigla = [] #lista para guardar o 1º nome e a 1ª letras dos sobrenomes
+nome_completo = nome_completo.split() #função para transformar o nome digitado pelo usuário em uma lista
+nome_sigla = [nome_completo[0]] #lista para guardar o 1º nome e a 1ª letras dos sobrenomes
 
-nome_completo.split() #função para transformar o nome digitado pelo usuário em uma lista
-
+'''
 for nome in nome_completo: #ciclo for para percorrer a lista com o nome digitado
     if nome == ' ': #condicional para captar a 1ª letra do sobrenome (uso do ' '(espaço como referência))
         sepador = nome_completo.index(nome) + 1 #a variável separador guarda a posição da letra seguinte ao espaço
@@ -12,4 +12,11 @@ for nome in nome_completo: #ciclo for para percorrer a lista com o nome digitado
         print(sepador) #mostra quanto tá valendo a variável separador
         print(nome_sigla) #mostra o conteúdo da lista nome_sigla
         print(nome_completo) #mostra o conteúdo da lista nome_completo
-    
+'''
+contador = 0
+for nome in nome_completo:
+    if nome not in nome_sigla:
+        nome_sigla.append(nome_completo[contador])
+    contador += 1
+
+print(nome_sigla)
