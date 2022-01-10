8e665341b3b0b848c15c36dd146983464db8d672
Arlindojulio <arlindojr25@hotmail.com>
2022-01-10 21:42:06 +0000

    Finalização do programa calculador de área.

--- calcula_geometria.py
+++ calcula_geometria.py
@@ -8,4 +8,14 @@ user = Calculador(int(input('Escolha um número correspondente as opções acima
 if user.opcao == 1:
     user.calc_area_quadrado()
 elif user.opcao == 2:
+    user.calc_area_retangulo()
+elif user.opcao == 3:
     user.calc_area_triagulo()
+elif user.opcao == 4:
+    user.calc_area_trapezio()
+elif user.opcao == 5:
+    user.calc_area_circulo()
+elif user.opcao == 6:
+    user.sair()
+else:
+    print('ERRO! Você não escolheu uma das opções do MENU.')
--- calculador_geometrico.py
+++ calculador_geometrico.py
@@ -10,8 +10,33 @@ class Calculador:
         print(f'A área total do quadrado é {area_quadrado}cm2.')
 
 
+    def calc_area_retangulo(self):
+        base = float(input('Digite o valor(cm) correspondente a base do retângulo: '))
+        altura = float(input('Digite o valor(cm) correspondente a altura do retângulo: '))
+        area_retangulo = base * altura
+        print(f'A área total do retangulo é {area_retangulo}cm2.')
+
+
     def calc_area_triagulo(self):
         base = float(input('Digite o valor(cm) correspondente a base do triângulo: '))
         altura = float(input('Digite o valor(cm) correspondente a altura do triângulo: '))
         area_triangulo = (base * altura) / 2
         print(f'A área total do triângulo é {area_triangulo}cm2.')
+
+
+    def calc_area_trapezio(self):
+        base1 = float(input('Digite o valor(cm) correspondente a primeira base do trapézio: '))
+        base2 = float(input('Digite o valor(cm) correspondente a segunda base do trapézio: '))
+        altura = float(input('Digite o valor(cm) correspondente a altura do retângulo: '))
+        area_trapezio = ((base1 + base2) / 2) * altura
+        print(f'A área total do trapézio é {area_trapezio}cm2.')
+
+
+    def calc_area_circulo(self):
+        raio = float(input('Digite o valor(cm) correspondente ao raio do circulo: '))
+        area_circulo = (3.14 * (raio ** 2))
+        print(f'A área total do circulo é {area_circulo}cm2.')
+
+
+    def sair(self):
+        pass
